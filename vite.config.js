32f8d14286dd85b3a2b180f834d0cffcfcbbcc41
Agnyathavasi <vishwajithhh12345@gmail.com>
2025-05-05 01:10:29 +0530

    for production

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,19 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/whatsapp/",
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: "js/[name]-[hash].js",
+  //       chunkFileNames: "js/[name]-[hash].js",
+  //       assetFileNames: ({ name }) => {
+  //         if (name && name.endsWith(".css")) {
+  //           return "css/[name]-[hash][extname]";
+  //         }
+  //         return "assets/[name]-[hash][extname]";
+  //       },
+  //     },
+  //   },
+  // },
 });
